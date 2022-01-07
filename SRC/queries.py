f4630ce228d1e413db48e7359248e83452dfc4dc
Yonatan33 <yoni.garti@gmail.com>
2022-01-07 08:03:19 +0200

    added query_2 for submission

--- SRC/queries.py
+++ SRC/queries.py
@@ -26,6 +26,19 @@ def query_1(user_from, user_to):
         print(f'movie_id: {movie_id} | title: {title} | release_date: {year}')
 
 
+# *********** Query2 - top movies by revenue ***********
+def query_2():
+    query = f'''select md.title, mp.revenue - mp.budget as profit
+                from movie_details as md, movie_profits as mp
+                where md.movie_id = mp.movie_id
+                order by (profit) desc
+                limit 20
+                '''
+    answer = send_execute(query)
+    for title, profit in answer:
+        print(f'title: {title} | Profits: {str(profit // 1000000) + "M$" if profit > 1000000  else str(profit // 100000) + "K$"}')
+
+
 # *********** Query3 - top movie genre by rating ***********
 def query_3():
     query = '''select distinct mg.genre_name, count(*)
@@ -139,26 +152,28 @@ def query_9():
 
 
 if __name__ == "__main__":
-    print(f'**** Query 1 execution****')
-    user_from, user_to = input("Enter year to start the search from"), input("Enter year to end the search at")
-    query_1(user_from, user_to)
-    print(f'**** Query 3 execution****')
-    query_3()
-    print(f'**** Query 4 execution****')
-    query_4()
-    print(f'**** Query 5 execution****')
-    query_5()
-    print(f'**** Query 6 execution****')
-    genre_name = input("Enter genre name")
-    query_6(genre_name)
-    print(f'**** Query 7 execution****')
-    genre_name = input("Enter genre name")
-    query_7(genre_name)
-    print(f'**** Query 8 execution****')
-    actor1 = input("Enter first actor's name").split(" ")
-    assert len(actor1) == 2, 'Name must be name and last name'
-    actor2 = input("Enter second actor's name").split(" ")
-    assert len(actor1) == 2, 'Name must be name and last name'
-    query_8(actor1[0], actor1[1], actor2[0], actor2[1])
-    print(f'**** Query 9 execution****')
-    query_9()
+    # print(f'**** Query 1 execution****')
+    # user_from, user_to = input("Enter year to start the search from"), input("Enter year to end the search at")
+    # query_1(user_from, user_to)
+    print(f'**** Query 2 execution****')
+    query_2()
+    # print(f'**** Query 3 execution****')
+    # query_3()
+    # print(f'**** Query 4 execution****')
+    # query_4()
+    # print(f'**** Query 5 execution****')
+    # query_5()
+    # print(f'**** Query 6 execution****')
+    # genre_name = input("Enter genre name")
+    # query_6(genre_name)
+    # print(f'**** Query 7 execution****')
+    # genre_name = input("Enter genre name")
+    # query_7(genre_name)
+    # print(f'**** Query 8 execution****')
+    # actor1 = input("Enter first actor's name").split(" ")
+    # assert len(actor1) == 2, 'Name must be name and last name'
+    # actor2 = input("Enter second actor's name").split(" ")
+    # assert len(actor1) == 2, 'Name must be name and last name'
+    # query_8(actor1[0], actor1[1], actor2[0], actor2[1])
+    # print(f'**** Query 9 execution****')
+    # query_9()
